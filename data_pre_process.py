8e51ec3126d71c0ed3e1652bc01ddcf245972bd5
Potladurthy-Sai-Praneeth <pspraneeth99@gmail.com>
2025-09-23 16:27:47 -0700

    Finished data pre processing

---
+++ data_pre_process.py
@@ -0,0 +1,78 @@
+from datasets import load_dataset
+from dotenv import load_dotenv
+import os
+from PIL import Image
+from tqdm import tqdm
+import multiprocessing as mp
+
+load_dotenv()
+BATCH_SIZE = int(os.getenv("BATCH_SIZE", 32)) 
+
+# System message for the assistant
+system_message = "You are an expert in Medical Diagnosis and treatment. You are given a image. You need to provide a description of the disease or anamoly or sometimes suggest possible treatments analysing the image."
+
+SYSTEM_MESSAGE_CONTENT = [{"type": "text", "text": system_message}]
+    
+def format_data(sample):
+    '''
+    This function returns a dictionary formatted for training/fine-tuning a multimodal model.
+    '''
+    return {
+        "messages": [
+            {
+                "role": "system",
+                "content": SYSTEM_MESSAGE_CONTENT,
+            },
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": sample['question']},
+                    {"type": "image", "image": sample["image"]},
+                ],
+            },
+            {
+                "role": "assistant",
+                "content": [{"type": "text", "text": sample["answer"]}],
+            },
+        ],
+    }
+
+
+def process_dataset(dataset):
+    '''
+    This function processes the dataset to create a list of formatted samples.
+    Each sample contains an image, a question, and an answer.
+    Note: dataset.map is not used as it serializes the image object which is not desired.
+    '''
+    processed_data = []
+
+    for point in dataset:
+        image,questions,answers = point['image'],point['questions'],point['answers']
+        for pair in zip(questions,answers):
+            question,answer = pair
+            sample = {
+                "image": image,
+                "question": question,
+                "answer": answer
+            }
+            formatted_sample = format_data(sample)
+            processed_data.append(formatted_sample)
+    
+    return processed_data
+
+# if __name__ == "__main__":
+#     load_dotenv()
+#     DATASET_ID = os.getenv("DATASET_ID")
+    
+#     print(f"Loading dataset: {DATASET_ID}")
+#     dataset = load_dataset(DATASET_ID, split="train")
+#     dataset = dataset.select(50)
+
+#     print(f"Dataset loaded with {len(dataset)} samples")
+
+#     processed_data = process_dataset(dataset)
+
+#     print(f"Processed {len(processed_data)} samples.")
+#     print("Sample processed data:")
+#     print(processed_data[0])
+
