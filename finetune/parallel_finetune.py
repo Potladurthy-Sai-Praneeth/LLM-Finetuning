090f1ad06bfc16af12d6bf751c106f8e9c53a153
Potladurthy-Sai-Praneeth <pspraneeth99@gmail.com>
2025-11-09 12:22:36 -0800

    removed modules to save

--- finetune/parallel_finetune.py
+++ finetune/parallel_finetune.py
@@ -145,7 +145,6 @@ class Trainer:
             num_train_epochs=int(self.config['training']['NUM_TRAIN_EPOCHS']),
             per_device_train_batch_size=effective_batch_size,
             gradient_accumulation_steps=gradient_accumulation_steps,
-            # gradient_checkpointing=True,
             gradient_checkpointing_kwargs = {"use_reentrant": False},
             optim="adamw_torch_fused",
             logging_steps=int(self.config['training']['LOGGING_STEPS']),
@@ -163,7 +162,6 @@ class Trainer:
             fsdp_config={
                 'fsdp_transformer_layer_cls_to_wrap': ['Gemma3DecoderLayer'],
                 # 'fsdp_transformer_layer_cls_to_wrap': ['PaliGemmaModel'],
-                'fsdp_activation_checkpointing': False,
                 **self.config['fsdp']
             }
         )
